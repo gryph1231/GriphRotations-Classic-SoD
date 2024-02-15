2e7ee2eb7681fd8863ca9e619c12fb97661b342d
gryph1231 <cliffordhill24@gmail.com>
2024-02-15 17:04:45 -0600

    Update Hunter.lua

--- Rubim-RH-Classic/Rotations/Hunter.lua
+++ Rubim-RH-Classic/Rotations/Hunter.lua
@@ -25,14 +25,29 @@ RubimRH.Spell[3] = {
 	HeartoftheLionz = Spell(20190), --aspect of the wild
 	AutoShotz = Spell(19263), --deterrence
 	AutoAttack = Spell(6603),
+	MendPet = Spell(3662),
+	MendPetz = Spell(20549), --war stomp
+	Carve = Spell(425711),
+    WyvernSting = Spell(19386),
+	AspectoftheViper = Spell(415423),
+	AspectoftheViperz = Spell(13161), --aspect of the beast
+	AspectoftheCheetah = Spell(5118),
+	AspectoftheCheetahCancel = Spell(19878), --track demons
+	Carvez = Spell(20594), --stoneform
 	AspectoftheHawk = Spell(13165),
 	ArcaneShot = Spell(3044),
 	FlankingStrike = Spell(415320),
-	FlankingStrikez = Spell(19306), --counterattack
+	FlankingStrikez = Spell(16832), --pet claw
+	Counterattack = Spell(19306),
 	SerpentSting = Spell(13550),
+	WingClip = Spell(14267),
+	ConcussiveShot = Spell(5116),
 	FeedPet = Spell(6991),
+	Flare = Spell(1543),
 	FeedPetBuff = Spell(1539),
 	FeignDeath = Spell(5384),
+	Disengage = Spell(14272),
+	BloodFury = Spell(20572),
 };
 
 local S = RubimRH.Spell[3]
@@ -40,12 +55,11 @@ local S = RubimRH.Spell[3]
 if not Item.Hunter then
     Item.Hunter = {}
 end
+
 Item.Hunter.BeastMastery = {
 };
 local I = Item.Hunter.BeastMastery;
 
-
-S.AimedShot.TextureSpellID = { 1 }
 S.AutoShot:RegisterInFlight()
 S.SerpentSting:RegisterInFlight()
 
@@ -61,41 +75,16 @@ local function bool(val)
     return val ~= 0
 end
 
-local function RangeCount(range_check)
-local range_counter = 0
-    
-	if range_check then
-		for i=1,40 do
-			local unitID = "nameplate" .. i
-			if UnitExists("nameplate"..i) then           
-				local nameplate_guid = UnitGUID("nameplate"..i) 
-				local npc_id = select(6, strsplit("-",nameplate_guid))
-				if npc_id ~='120651' and npc_id ~='161895' then
-					if UnitCanAttack("player",unitID) and IsActionInRange(range_check,unitID) and UnitHealthMax(unitID) > 5
-					and UnitName(unitID) ~= "Incorporeal Being" then
-						range_counter = range_counter + 1
-					end                    
-				end
-			end
-		end
-	else
-		range_counter = 0
-	end
-    
-    return range_counter
-end
-
 local function CleaveCount()
 local cleave_counter = 0
-     
+
         for i=1,40 do
 			local unitID = "nameplate" .. i
             if UnitExists("nameplate"..i) then           
                 local nameplate_guid = UnitGUID("nameplate"..i) 
                 local npc_id = select(6, strsplit("-",nameplate_guid))
                 if npc_id ~='120651' and npc_id ~='161895' then
-                    if UnitCanAttack("player",unitID) and IsActionInRange(37,unitID) and UnitHealthMax(unitID) > 5
-					and UnitName(unitID) ~= "Incorporeal Being"	then
+                    if UnitCanAttack("player",unitID) and IsActionInRange(37,unitID) and UnitHealthMax(unitID) > 5 then
                         cleave_counter = cleave_counter + 1
                     end                    
                 end
@@ -105,19 +94,7 @@ local cleave_counter = 0
     return cleave_counter
 end
 
-local function TargetinRange(range_check)
-	if range_check then
-		if IsActionInRange(range_check,"target") then
-			return true
-		else
-			return false
-		end
-	else
-		return false	
-	end
-end
-
-local function stingTime()
+local function StingTime()
 local _,_,_,_,_,expirationTime = AuraUtil.FindAuraByName("Serpent Sting","target","PLAYER|HARMFUL")
 
 if expirationTime then
@@ -132,9 +109,6 @@ local function PetHapiness()
 local happiness, damagePercentage, loyaltyRate = GetPetHappiness()
 	local happy = ({"Unhappy", "Content", "Happy"})[happiness]
 	local loyalty = loyaltyRate > 0 and "gaining" or "losing"
-	-- print("Pet is "..happy)
-	-- print("Pet is doing "..damagePercentage.."% damage")
-	-- print("Pet is "..loyalty.." loyalty")
 	return happy
 end
 
@@ -144,6 +118,7 @@ end
 
 local function PetActive()
     local petActive = false
+	
     if Pet:Exists() then
         petActive = true
     end
@@ -157,58 +132,6 @@ local function PetActive()
     end
 
     return petActive
-end
-
-local function SwingTime()
-	--1.11 = quiver speed
-	local haste = 1.11 * (1 + GetRangedHaste()/100)
-	local ASCast = 0.5 / haste
-	
-	-- speed, lowDmg, hiDmg, posBuff, negBuff, percent = UnitRangedDamage("player");
-	-- local f = CreateFrame("Frame")
-		-- f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-		-- f:SetScript("OnEvent", function(self, event)
-
-		-- local _, subevent, _, sourceGUID, _, _, _, _, destName = CombatLogGetCurrentEventInfo()
-
-		-- local spellId, _, _, amount, _, _, _, _, _, critical = select(12, CombatLogGetCurrentEventInfo())
-		
-		
-		-- if subevent == "SPELL_CAST_START" and spellId == 75 then
-			-- AutoCheck = GetTime()
-		-- end
-	-- end)
-	
-	-- if AutoCheck and AutoCheck > 0 then
-		-- return (speed - ASCast) - (GetTime() - AutoCheck)
-	-- else
-		-- return 0
-	-- end
-	
-	-- FD resets swing timer**
-	-- if S.FeignDeath:CooldownRemains() >= 30 - (UnitRangedDamage("player") - ASCast) then 
-		-- Swing = (UnitRangedDamage("player") - ASCast) - (30 - S.FeignDeath:CooldownRemains()) 
-	-- else
-	local Swing = (UnitRangedDamage("player") - ASCast) - S.AutoShot:TimeSinceLastCast()
-	--end
-	
-	if Swing and Swing > 0 then
-		return Swing
-	else
-		return 0
-	end
-end
-
-local function SpellRank(spell_name)
-
-	if spell_name then
-		local _,_,_,_,_,_,spellID,_ = GetSpellInfo(spell_name)
-		
-		return spellID
-	else
-		return 0
-	end
-
 end
 
 local function IsReady(spell,range_check,aoe_check)
@@ -282,46 +205,22 @@ local function IsReady(spell,range_check,aoe_check)
 	end
 end
 
-local lastMoveTime = GetTime()  -- Initialize with the current time
-
--- Function to get the time since last moved
-local function TimeSinceLastMoved()
-    return GetTime() - lastMoveTime
-end
-
--- Frame to track player movement
-local moveFrame = CreateFrame("Frame")
-moveFrame:SetScript("OnUpdate", function(self, elapsed)
-    local currentSpeed = GetUnitSpeed("player")
-    if currentSpeed > 0 then
-        -- Update the last move time to the current time if the player is moving
-        lastMoveTime = GetTime()
-    end
-end)
-
 local function APL()
-SwingTime()
-RangeCount()
-TargetinRange()
 CleaveCount()
 IsReady()
 ManaPct()
 PetActive()
 PetHapiness()
-stingTime()
-SpellRank()
-TimeSinceLastMoved()
-
-
+StingTime()
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+--Functions/Top priorities-----------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------
 if UnitCastingInfo('Player') or UnitChannelInfo('Player') or IsCurrentSpell(19434) then
 	return "Interface\\Addons\\Rubim-RH-Classic\\Media\\channel.tga", false
 elseif Player:IsDeadOrGhost() or AuraUtil.FindAuraByName("Drink", "player") or AuraUtil.FindAuraByName("Food", "player") or AuraUtil.FindAuraByName("Food & Drink", "player") then
 	return "Interface\\Addons\\Rubim-RH-Classic\\Media\\mount2.tga", false
-end 
+end
 
---1.11 = quiver speed
-local MultiShotTime = 0.5 / (1.11 * (1 + GetRangedHaste() / 100))
-local AimedShotTime = 3 / (1.11 * (1 + GetRangedHaste() / 100))
 local AutoShot = 0
 
 for ActionSlot = 1, 120 do
@@ -332,47 +231,110 @@ for ActionSlot = 1, 120 do
 		AutoShot = ActionSlot
 	end
 end
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+--Spell Queue-----------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+if RubimRH.queuedSpell[1]:CooldownRemains() > 2 or not RubimRH.queuedSpell[1]:IsAvailable() or not UnitAffectingCombat('player')
+	or (S.AspectoftheCheetah:ID() == RubimRH.queuedSpell[1]:ID() and AuraUtil.FindAuraByName("Aspect of the Cheetah", "player"))
+	or (S.ConcussiveShot:ID() == RubimRH.queuedSpell[1]:ID() and CheckInteractDistance("target",3))	then
+	RubimRH.queuedSpell = { RubimRH.Spell[3].Default, 0 }
+end
 
+if S.Flare:ID() == RubimRH.queuedSpell[1]:ID() and S.Flare:CooldownRemains() < 2 then
+	return S.Flare:Cast()
+end
+
+if S.AspectoftheCheetah:ID() == RubimRH.queuedSpell[1]:ID() and S.AspectoftheCheetah:CooldownRemains() < 2 then
+	return S.AspectoftheCheetah:Cast()
+end
+
+if S.BloodFury:ID() == RubimRH.queuedSpell[1]:ID() and S.BloodFury:CooldownRemains() < 2 then
+	return S.BloodFury:Cast()
+end
+
+if S.ConcussiveShot:ID() == RubimRH.queuedSpell[1]:ID() and S.ConcussiveShot:CooldownRemains() < 2 then
+	return S.ConcussiveShot:Cast()
+end
+
+if S.WingClip:ID() == RubimRH.queuedSpell[1]:ID() and S.WingClip:CooldownRemains() < 2 then
+	return S.WingClip:Cast()
+end
+
+if RubimRH.QueuedSpell():CanCast() then
+	return RubimRH.QueuedSpell():Cast()
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+--Out of Combat-----------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------
 if not Player:AffectingCombat() then
 	if IsReady('Feed Pet') and (PetHapiness() == 'Content' or PetHapiness() == 'Unhappy') and PetActive() and not Pet:Buff(S.FeedPetBuff) then 
 		return S.FeedPet:Cast() 
 	end
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+--Rotation-----------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------
+if Player:AffectingCombat() and CleaveCount() >= 1 
+and (AuraUtil.FindAuraByName("Aspect of the Cheetah", "player") 
+or (AuraUtil.FindAuraByName("Dazed", "player") and UnitAffectingCombat('player'))) then
+	return S.AspectoftheCheetahCancel:Cast()
 end
 
 if IsReady('Heart of the Lion') and not Player:Buff(S.HeartoftheLion) then
 	return S.HeartoftheLionz:Cast()
 end
 
-if IsReady('Aspect of the Hawk') and not AuraUtil.FindAuraByName("Aspect of the Hawk", "player")
-and not AuraUtil.FindAuraByName("Aspect of the Cheetah", "player")
-and not AuraUtil.FindAuraByName("Aspect of the Monkey", "player") then
-	return S.AspectoftheHawk:Cast()
+if IsReady('Aspect of the Viper') and ManaPct() < 10 and not AuraUtil.FindAuraByName("Aspect of the Viper", "player") 
+and ((not AuraUtil.FindAuraByName("Aspect of the Cheetah", "player") and not AuraUtil.FindAuraByName("Aspect of the Pack", "player")) 
+or (UnitAffectingCombat('player') and CheckInteractDistance("target",3))) then
+	return S.AspectoftheViperz:Cast()
 end
 
 if UnitCanAttack('player', 'target') and (UnitAffectingCombat('target') or IsCurrentSpell(6603) or IsAutoRepeatAction(AutoShot)) and not Target:IsDeadOrGhost() then 
-	if not IsAutoRepeatAction(AutoShot) and IsSpellInRange('Auto Shot', 'target') == 1 then
+	if not IsAutoRepeatAction(AutoShot) and not Player:IsMoving() and IsSpellInRange('Auto Shot', 'target') == 1 then
 		return S.AutoShotz:Cast()
 	end
 
 	if not IsCurrentSpell(6603) and CheckInteractDistance("target",3) then
 		return Item(135274, { 13, 14 }):ID()
 	end
-	
-	if IsReady('Raptor Strike') and CheckInteractDistance("target",3) and not IsCurrentSpell(SpellRank('Raptor Strike')) and IsCurrentSpell(6603) then
-		return S.RaptorStrike:Cast()
+
+	if IsReady('Counterattack') and CheckInteractDistance("target",3) and UnitIsPlayer("target") then
+		return S.Counterattack:Cast()
+	end
+
+	if IsReady('Carve') and CheckInteractDistance("target",3) and CleaveCount() >= 3 then
+		return S.Carvez:Cast()
 	end
 	
 	if IsReady('Flanking Strike',1) and CheckInteractDistance("target",3) then
 		return S.FlankingStrikez:Cast()
 	end
 
-	if IsReady('Multi-Shot',1) and not Player:IsMoving() and SwingTime() > MultiShotTime then
+	if IsReady('Raptor Strike') and CheckInteractDistance("target",3) then
+		return S.RaptorStrike:Cast()
+	end
+
+	if IsReady('Carve') and CheckInteractDistance("target",3) and IsCurrentSpell(6603) then
+		return S.Carvez:Cast()
+	end
+
+	if IsReady('Aspect of the Hawk') and ManaPct() > 20 and (S.RaptorStrike:CooldownRemains() > 1 and S.FlankingStrike:CooldownRemains() > 1 and (S.Carve:CooldownRemains() > 1 or CleaveCount() < 3))
+	and not AuraUtil.FindAuraByName("Aspect of the Hawk", "player")
+	and not AuraUtil.FindAuraByName("Aspect of the Cheetah", "player")
+	and not AuraUtil.FindAuraByName("Aspect of the Pack", "player")
+	and (not AuraUtil.FindAuraByName("Aspect of the Viper", "player") or ManaPct() == 100)
+	and not AuraUtil.FindAuraByName("Aspect of the Monkey", "player") then
+		return S.AspectoftheHawk:Cast()
+	end
+
+	if IsReady('Multi-Shot',1) and not Player:IsMoving() then
 		return S.Multishot:Cast()
 	end
 
 	if IsReady('Serpent Sting',1) and not S.SerpentSting:InFlight() and RubimRH.CDsON() 
 	and (not AuraUtil.FindAuraByName("Serpent Sting","target","PLAYER|HARMFUL") 
-	or (AuraUtil.FindAuraByName("Serpent Sting","target","PLAYER|HARMFUL") and stingTime() <= Player:GCD())) then
+	or (AuraUtil.FindAuraByName("Serpent Sting","target","PLAYER|HARMFUL") and StingTime() <= Player:GCD())) then
 		return S.SerpentSting:Cast()
 	end
 
@@ -380,24 +342,13 @@ if UnitCanAttack('player', 'target') and (UnitAffectingCombat('target') or IsCur
 		return S.ChimeraShotz:Cast()
 	end
 
-	if IsReady('Aimed Shot',1) and RubimRH.CDsON() and TimeSinceLastMoved() > 0.5
-	and (SwingTime() > AimedShotTime or S.AutoShot:InFlight()) then
-		return S.AimedShot:Cast()
-	end
-
-	if IsReady('Arcane Shot',1) and RubimRH.CDsON() and (Player:MovingFor() > 0.5 or not IsReady('Aimed Shot',1)) then
+	if IsReady('Arcane Shot',1) and RubimRH.CDsON() then
 		return S.ArcaneShot:Cast()
 	end
 end
 
 	return "Interface\\Addons\\Rubim-RH-Classic\\Media\\mount2.tga", false
 end
-
--- TimeSinceLastCast() <= gcd + casttime?
--- swing timer total - TimeSinceLastCast() = swing time remaining
--- swing timer total = tooltip atk speed - 0.5 (atk cast) - UnitRangedDamage("player")
--- (UnitRangedDamage("player") - 0.5) - TimeSinceLastCast() = swing time remaining
-
 
 RubimRH.Rotation.SetAPL(3, APL);
 RubimRH.Rotation.SetPvP(3, PvP)
